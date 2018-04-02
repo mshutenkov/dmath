065a1a14b798ba703e9c76463bd8fe867e6bb975
Mikhail Shutenkov <shutenkov.m@gmail.com>
2018-04-02 19:50:02 +0300

    improve polynom string representation

--- pretty.py
+++ pretty.py
@@ -1,5 +1,5 @@
 import common
-from rational import TRANS_Q_Z
+from rational import TRANS_Q_Z, INT_Q_B
 
 
 def rational(Q):
@@ -14,10 +14,18 @@ def rational(Q):
 def polynom(P):
     result = ''
 
-    for rank in range(P[0]):
+    for rank in range(P[0] + 1):
         coef = P[1][rank]
-        if coef[0][1] != 0 or coef[0][2][0] != 0:
-            elem = '%s' % rational(coef)
+        if coef[0][1] != 1 or coef[0][2][0] != 0:
+            if INT_Q_B(coef):
+                num = common.Z_to_num(TRANS_Q_Z(coef))
+                if num != 1:
+                    elem = '%d' % num
+                else:
+                    elem = ''
+            else:
+                elem = '%s' % rational(coef)
+
             if rank:
                 elem += 'x'
                 if rank > 1:
