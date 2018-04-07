99583ee737bf159439a22e7b5fc7b200c94fcd81
Mikhail Shutenkov <shutenkov.m@gmail.com>
2018-04-07 19:45:32 +0300

    add Z-10 P-11 modules

--- integer/MOD_ZZ_Z.py
+++ integer/MOD_ZZ_Z.py
@@ -1,4 +1,5 @@
 from integer import DIV_ZZ_Z, MUL_ZZ_Z, SUB_ZZ_Z, MUL_ZM_Z
+import common
 
 
 def MOD_ZZ_Z(number1, number2):
@@ -6,8 +7,11 @@ def MOD_ZZ_Z(number1, number2):
     Z-10
     Остаток от деления большего целого числа на меньшее
     или равное натуральное с остатком (делитель отличен от нуля)
-    %author%, 7305
+    Рэшин Даниил, 7305
     """
-    # TODO
-    return number1
+    # Частное
+    d = DIV_ZZ_Z(number1, number2)
+
+    p = MUL_ZZ_Z(number2, d)
+    return SUB_ZZ_Z(number1, p)
 
--- polynom/GCF_PP_P.py
+++ polynom/GCF_PP_P.py
@@ -1,11 +1,25 @@
-from polynom import DEG_P_N, MOD_PP_P
+from polynom import DEG_P_N, MOD_PP_P, MUL_PQ_P
+from rational import DIV_QQ_Q
+from integer import POZ_Z_D
+
+import common
 
 
 def GCF_PP_P(polynom1, polynom2):
     """
     P-11
     НОД многочленов
-    %author%, 7305
+    Рэшин Даниил, 7305
     """
-    return None
+    one = polynom1
+    two = polynom2
+
+    # Алгоритм Евклида
+    while DEG_P_N(two) > 0 or POZ_Z_D(two[1][0][0]) != 0:
+        two, one = MOD_PP_P(one, two), two
+
+    # Делим на старший коэффициент для получения единицы
+    first = common.copy_Q(one[1][-1])
+    first = DIV_QQ_Q(common.rat_to_Q(1, 1), first)
+    return MUL_PQ_P(one, first)
 
